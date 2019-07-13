c840b2365964b9ce8df00488ac2d34bff09e6a13
endzi007 <jasarovic.enis@gmail.com>
2019-07-13 16:06:22 +0200

    working on 720 download quality

--- src/main/ffmpegProcesses/customRangeDownload.js
+++ src/main/ffmpegProcesses/customRangeDownload.js
@@ -1,18 +1,49 @@
 import path from 'path';
 import { execFile } from 'child_process';
+import { unwatchFile } from 'fs';
+import { reduce } from 'bluebird-lst';
 
 export default (store, index)=>{
     return new Promise((resolve, reject)=>{
         let state = store.getState();
+        const { downloadFormat } = state.options;
         let stateVideo = state.videos[index];
-        let formatsToDownload = getFormatsToDownload(state.options.downloadFormat, stateVideo.downloadLinks);
+        let formatsToDownload = getFormatsToDownload(downloadFormat, stateVideo.downloadLinks);
         let modifiedTitle = stateVideo.title.replace(/[|*:/"<>,]/g, "-");
         let getPercent = (stateVideo.range.range[1]/stateVideo.duration)*100;
-        console.log("percent", stateVideo.range.range[1], stateVideo.range.range[0]);
         let sizeToDownload = ((formatsToDownload.filesize / 100) * getPercent)/1024;
-        let baseValue = 0;
-        let video = execFile(path.resolve(__static, "ffmpeg"), 
-        ['-ss', stateVideo.range.range[0], '-i', formatsToDownload.url, "-y", "-loglevel", "quiet", "-stats", '-t', stateVideo.range.range[1], '-c:a', "copy",  modifiedTitle+"."+formatsToDownload.ext]);
+        let args;
+        let qa;
+        if(downloadFormat.type === "mp3"){
+            if(downloadFormat.quality === "low"){
+                qa = 9;
+            } else if(downloadFormat.quality === "medium"){
+                qa = 5
+            } else {
+                qa = 0;
+            }
+        }
+        if(downloadFormat.type === "mp3"){
+            args = ['-ss', stateVideo.range.range[0], 
+            '-i', formatsToDownload.url, 
+            "-y","-stats", 
+            '-t', stateVideo.range.range[1], 
+            '-c:a', "libmp3lame", 
+            "-q:a", qa,  
+            modifiedTitle+"."+downloadFormat.type]
+        } else {
+            args = ['-ss', stateVideo.range.range[0], 
+            '-i', formatsToDownload.url, 
+            "-y", "-stats", 
+            '-t', stateVideo.range.range[1], 
+            "-c:v", "copy",
+            '-c:a', "copy",
+            modifiedTitle+"."+downloadFormat.type]
+
+        }
+        console.log(args);
+
+        let video = execFile(path.resolve(__static, "ffmpeg"), [...args]);
         video.stdout.on("data", (data)=>{
             console.log("DATA", data);
         })
@@ -40,26 +71,31 @@ export default (store, index)=>{
 }
 
 function getFormatsToDownload (formatObj, formats){
-    switch (formatObj.type) {
-        case "mp3":
-            let reducedFormats = []; 
+    let reducedFormats = []; 
             formats.forEach(format => {
-                if(format.vcodec === "none"){
-                    reducedFormats.push(format);
-                } 
-            });;
-            if(formatObj.quality === "low"){
-                return reducedFormats[0];
-            } else if(formatObj.quality === "medium"){
-
-                return reducedFormats[Math.ceil(reducedFormats.length/2)];
-            } else {
-
-                return reducedFormats[reducedFormats.length-1];
-            }
-        case "mp4":
-            break;
-        default:
-            break;
-    }
+                if(format.format_id === "18"){
+                    if(formatObj.type === "mp3"){
+                        reducedFormats.unshift(format);
+                    } else {
+                        if(formatObj.quality === "360"){
+                            reducedFormats.unshift(format);
+                        } else {
+                            reducedFormats.push(format);
+                        }
+                    }
+                }
+                else if(format.format_id === "22"){
+                    if(formatObj.type === "mp3") {
+                        reducedFormats.push(format);
+                    } else {
+                        if(formatObj.quality === "720"){
+                            reducedFormats.unshift(format);
+                        } else {
+                            reducedFormats.push(format);
+                        }
+                    }
+                }
+            });
+               
+            return reducedFormats[0];
 }
